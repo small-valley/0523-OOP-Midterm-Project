6ed29824ffeec1594a770f102b4204a322b62c93
small-valley <kuzan1123@gmail.com>
2023-09-19 13:26:30 -0700

    add Step2 - Product Item

---
+++ assets/scripts/product-item.js
@@ -0,0 +1,24 @@
+export class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+    addToCart() {
+        console.log(this.product);
+    }
+    render() {
+        return `<li class="product-item">
+            <div>
+                <img
+                    src="${this.product.image}"
+                    alt="${this.product.title}"
+                />
+                <div class="product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                    <button id="product-item-${this.product.id}">Add to Cart</button>
+                </div>
+            </div>
+        </li>`;
+    }
+}
