c10bad5a6edc6e48729ad251994f75c18d44360c
small-valley <kuzan1123@gmail.com>
2023-09-25 12:38:48 -0700

    fix css

--- assets/scripts/shop.js
+++ assets/scripts/shop.js
@@ -5,7 +5,9 @@ export class Shop {
     constructor() {}
     async render() {
         $("body").append(`<div id="app">
-            <section class="cart"></section>
+            <div id="header-container">
+                <header class="cart"></header>
+            </div>
             <ul class="product-list" id="prod-list"></ul>
         </div>`);
         await new ProductList().render();
